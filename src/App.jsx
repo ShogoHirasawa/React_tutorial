62aad9d0e358a83af8180d4c1c427faa94607a16
Shogo Hirasawa <origami.no.1@gmail.com>
2020-12-01 00:29:28 +0900

    useEffectの使い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+/*eslint react-hooks/exhaustive-deps:off*/
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  //useState一覧
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  //useState使用一覧
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
@@ -13,7 +32,10 @@ const App = () => {
 
       <ColorfulMessage color="pink">ごきげんよう</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br></br>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
