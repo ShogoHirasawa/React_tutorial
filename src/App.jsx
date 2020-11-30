37994e494edf2a282010feef3ac7c491f82cf690
Shogo Hirasawa <origami.no.1@gmail.com>
2020-11-30 22:02:39 +0900

    stateについて学んだ ボタンを押すとカウントアップする

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert("テストアラート");
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
-
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか </ColorfulMessage>
 
       <ColorfulMessage color="pink">ごきげんよう</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
